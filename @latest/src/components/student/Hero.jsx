326e91efbcbef4a4f63cb2d3b44ed1515e961d51
satvikyadav2021 <satvikyadav2021@gmail.com>
2025-06-30 16:58:19 +0000

    clerk added

--- @latest/src/components/student/Hero.jsx
+++ @latest/src/components/student/Hero.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 const Hero = () => {
   return (
     <div>
-        <h1>hero</h1>
+        <h1>
+           Unlock your potential with expert-led courses and 
+          <span>hands-on learning – anytime, anywhere.</span>
+        </h1>
     </div>
   )
 }
--- @latest/src/main.jsx
+++ @latest/src/main.jsx
@@ -4,14 +4,23 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx'
 import { AppContextProvider } from './context/AppContext.jsx'
 import {BrowserRouter, BrowserRouter as Router} from "react-router-dom"
+import {ClerkProvider} from '@clerk/clerk-react'
 
 const rootElement = document.getElementById('root')
 const root = ReactDOM.createRoot(rootElement)
 
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Missing Publishable Key')
+}
+
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <AppContextProvider>
-      <App />
-    </AppContextProvider>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl='/'>
+      <AppContextProvider>
+        <App />
+      </AppContextProvider>
+    </ClerkProvider>
   </BrowserRouter>
 )
